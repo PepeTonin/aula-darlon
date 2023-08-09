09e3a7794362dda373ae98cade075561ce771782
pedro <phavilat@hotmail.com>
2023-08-08 22:17:35 -0300

    add new features

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Hello from docker container!")
+
+with st.form(key='my form'):
+    submitted = st.form_submit_button('Click me')
+    if submitted:
+        st.balloons()
